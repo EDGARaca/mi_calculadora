7e6ada25214f84889fd8e131818ce03a33abd88d
EDGARaca <edgar.alfonso.ca@gmail.com>
2025-07-02 15:22:28 -0500

    Agregar función Elevar al cubo

--- calculadora.py
+++ calculadora.py
@@ -21,9 +21,10 @@ def potencia(a: float, b: float) -> float:
     return a**b
 
 if __name__ == "__main__":
-    print(sumar(2, 3))          #5
-    print(restar(5, 1))         #4
-    print(multiplicar(4, 2))    #8
-    print(dividir(10, 2))       #5.0
-    print(potencia(2,2))        #4.0
+    print(sumar(2, 3))              #5
+    print(restar(5, 1))             #4
+    print(multiplicar(4, 2))        #8
+    print(dividir(10, 2))           #5.0
+    print(potencia(2,2))            #4.0
+    
 
