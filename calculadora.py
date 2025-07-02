6e25a50223f45797df9fb91b4d1873052d1904c5
EDGARaca <edgar.alfonso.ca@gmail.com>
2025-07-02 12:06:29 -0500

    Agregar función de potencia

--- calculadora.py
+++ calculadora.py
@@ -16,8 +16,14 @@ def dividir(a: float, b: float) -> float:
         raise ValueError("No se puede dividir por cero.")
     return a / b
 
+def potencia(a: float, b: float) -> float:
+    #Calcula a elevado a la b.
+    return a**b
+
 if __name__ == "__main__":
     print(sumar(2, 3))          #5
     print(restar(5, 1))         #4
     print(multiplicar(4, 2))    #8
     print(dividir(10, 2))       #5.0
+    print(potencia(2,2))        #4.0
+
